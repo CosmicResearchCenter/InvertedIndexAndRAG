3ea7a5e72be21e003ad62c803dd4059ad06bbc71
markyangkp <markyangkp@outlook.com>
2024-09-22 21:16:37 +0800

    修复bug

--- backend/app/core/rag/models/source_document.py
+++ backend/app/core/rag/models/source_document.py
@@ -1,8 +1,16 @@
 from  pydantic import BaseModel
-class SourceDocument:
+class SourceDocument(BaseModel):
     content: str
     knowledge_doc_name: str
     
-    def __init__(self, content:str, knowledge_doc_name: str):
-        self.content = content
-        self.knowledge_doc_name = knowledge_doc_name
+#     def __init__(self, content:str, knowledge_doc_name: str):
+#         self.content = content
+#         self.knowledge_doc_name = knowledge_doc_name
+
+# class SourceDocument:
+#     content: str
+#     knowledge_doc_name: str
+    
+#     def __init__(self, content:str, knowledge_doc_name: str):
+#         self.content = content
+#         self.knowledge_doc_name = knowledge_doc_name
