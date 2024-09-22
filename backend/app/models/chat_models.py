f2f57b4ce7c447d3c01e6f5699fd414752bc0a67
markyangkp <markyangkp@outlook.com>
2024-09-22 11:14:31 +0800

    聊天请求体

--- backend/app/models/chat_models.py
+++ backend/app/models/chat_models.py
@@ -1,20 +1,79 @@
 from pydantic import BaseModel
-from typing import Any,List, Optional,Dict
+from typing import List, Optional,Dict
+from app.core.rag.models.knolwedge_base import ResultByDoc
 
-class Query(BaseModel):
-    query: str = None,
-    knowledge_base_id: str = None
-    user_id: str = None
-    conversation_id: str = None
-class Answer(BaseModel):
-    answer: str = None
-    conversation_id: str = None
-    def to_dict(self):
-        return {
-            "answer": self.answer,
-            "conversation_id": self.conversation_id
-        }
-class ResultByKnowledgeBase(BaseModel):
-    code:str
-    data:List[Dict]
-    message:str
+# Chat message request
+class ChatMessageRequest(BaseModel):
+    conversation_id: str
+    message: str
+    user_id: str
+
+# Chat message response
+# class ChatMessageResponseData(BaseModel):
+#     code: Optional[int]
+#     answer: Optional[str]
+#     Source: Optional[str]
+
+class ChatMessageResponse(BaseModel):
+    code: int
+    data: ResultByDoc
+    message: str
+
+# Knowledge base selection request
+class KnowledgeBaseSelectRequest(BaseModel):
+    conversation_id: str
+    knowledge_base_id: str
+
+# Knowledge base selection response
+class KnowledgeBaseSelectResponse(BaseModel):
+    code: int
+    data: List[Optional[dict]]
+    message: str
+
+# Clear chat request
+class ChatClearRequest(BaseModel):
+    conversation_id: str
+
+# Clear chat response
+class ChatClearResponse(BaseModel):
+    code: int
+    data: List[Optional[dict]]
+    message: str
+
+# Create knowledge base request
+class CreateKnowledgeBaseRequest(BaseModel):
+    name: str
+
+# Create knowledge base response
+class CreateKnowledgeBaseResponse(BaseModel):
+    code: int
+    data: List[Optional[dict]]
+    message: str
+
+# Knowledge base list response
+class KnowledgeBaseListResponse(BaseModel):
+    code: int
+    data: List[Optional[dict]]
+    message: str
+
+# Knowledge base details response
+class KnowledgeBaseDetailResponse(BaseModel):
+    code: int
+    data: List[Optional[dict]]
+    message: str
+
+# Upload file request
+class UploadFileRequest(BaseModel):
+    file: str
+
+# Upload file response
+class UploadFileResponse(BaseModel):
+    code: int
+    data: List[Optional[dict]]
+    message: str
+
+# File index status response
+class FileIndexStatusResponse(BaseModel):
+    code: int
+    data: List[Optional[dict]]
+    message: str
