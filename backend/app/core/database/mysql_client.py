cf8abc319a46cf85b5238ba3adb80f5899f4b603
markyangkp <markyangkp@outlook.com>
2024-09-22 11:14:19 +0800

    mysql链接

--- backend/app/core/database/mysql_client.py
+++ backend/app/core/database/mysql_client.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from typing import Any, Callable, Optional
-from .model import KnowledgeBasesList,ConversationsList
+from .models import KnowledgeBasesList,ConversationsList
 from typing import List
 # 数据库设置
 DATABASE_URL = "mysql+pymysql://llmqa_user:www123...@10.116.123.148:3308/llmqa"
@@ -15,30 +15,30 @@ class MysqlClient:
     def __init__(self,database_url:str=DATABASE_URL):
         self.engine = create_engine(database_url)
         self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine,expire_on_commit=False)
-    def GetConversationsList(self)->ConversationsList:
-        # if session is None:
-        session = self.SessionLocal()
-        conversations_list = session.query(ConversationsList).all()
-        session.close()
+#     def GetConversationsList(self)->ConversationsList:
+#         # if session is None:
+#         session = self.SessionLocal()
+#         conversations_list = session.query(ConversationsList).all()
+#         session.close()
 
-        return conversations_list
-    # 添加知识库Name和ID到表里
-    def AddKnowledgeBasesList(self,knowledgeBaseName:str)->KnowledgeBasesList:
-        # if session is None:
-        session = self.SessionLocal()
-        new_knowledge_base = KnowledgeBasesList(knowledgeBaseName=knowledgeBaseName)
-        session.add(new_knowledge_base)
-        session.commit()
-        session.close()
-        return new_knowledge_base
-    def GetKnowledgeBasesList(self)->List[KnowledgeBasesList]:
-        # if session is None:
-        session = self.SessionLocal()
-        knowledge_bases_list = session.query(KnowledgeBasesList).all()
-        session.close()
-        return knowledge_bases_list
+#         return conversations_list
+#     # 添加知识库Name和ID到表里
+#     def AddKnowledgeBasesList(self,knowledgeBaseName:str)->KnowledgeBasesList:
+#         # if session is None:
+#         session = self.SessionLocal()
+#         new_knowledge_base = KnowledgeBasesList(knowledgeBaseName=knowledgeBaseName)
+#         session.add(new_knowledge_base)
+#         session.commit()
+#         session.close()
+#         return new_knowledge_base
+#     def GetKnowledgeBasesList(self)->List[KnowledgeBasesList]:
+#         # if session is None:
+#         session = self.SessionLocal()
+#         knowledge_bases_list = session.query(KnowledgeBasesList).all()
+#         session.close()
+#         return knowledge_bases_list
 
-if __name__ == "__main__":
-    mysql_client = MysqlClient()
-    conversations_list = mysql_client.GetKnowledgeBasesList()
-    print(conversations_list[-1].knowledgeBaseName)
+# if __name__ == "__main__":
+#     mysql_client = MysqlClient()
+#     conversations_list = mysql_client.GetKnowledgeBasesList()
+#     print(conversations_list[-1].knowledgeBaseName)
