434ace07cbba7776767d89ad5979476794541c5c
markyangkp <markyangkp@outlook.com>
2024-09-30 20:21:12 +0800

    修改apikey和地址

--- backend/config/config.py
+++ backend/config/config.py
@@ -1,9 +1,9 @@
 from config.splitter_model import SplitterModel
 # OpenAI的BaseURL
-OPENAI_BASE_URL = "https://api.openai-proxy.org/v1"
+OPENAI_BASE_URL = "https://aihubmix.com/v1"
 
 # OpenAI的API_KEY
-OPENAI_API_KEY = "sk-Ob9W6r83cOj95sTXXMRPC61FDEIF6ZgoPEQur8PPyFrcuSKa"
+OPENAI_API_KEY = "sk-i2dHdOeKiGt9wA2G853b4dC10aEc419a9051F3C69e86284b"
 
 # 豆包的API_KEY
 DOUBAO_API_KEY = "d42e75b4-a5ef-4e9a-90ed-d93a9dcc2966"
