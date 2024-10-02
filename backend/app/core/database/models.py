5948137430e026020f4dd6a3328cb9ca31600f47
markyangkp <markyangkp@outlook.com>
2024-10-02 16:04:45 +0800

    新增一个文档信息表，并修改id生成方式

--- backend/app/core/database/models.py
+++ backend/app/core/database/models.py
@@ -11,6 +11,14 @@ def generate_id(length=18):
     # 截取指定长度
     return full_id[:length].replace('-', '')
 
+def generate_general_id(length=18):
+    # 生成一个 UUID
+    id_str = str(uuid.uuid4())
+    # 将 UUID 前加上 'kb'
+    full_id = f'uu{id_str}'
+    # 截取指定长度
+    return full_id[:length].replace('-', '')
+
 class KnowledgeBase(Base):
     __tablename__ = 'knowledgeBasesList'
     id = Column(String(18), primary_key=True, default=lambda: str(generate_id(length=18)))
@@ -19,7 +27,7 @@ class KnowledgeBase(Base):
 # 对话列表
 class Conversation(Base):
     __tablename__ = 'conversationsList'
-    id = Column(String(18), primary_key=True, default=lambda: str(generate_id(length=18)))
+    id = Column(String(18), primary_key=True, default=lambda: str(generate_general_id(length=18)))
     lastChatTime 	= Column(TIMESTAMP, nullable=False)
     conversationName 	= Column(String(255))
     num_conversation = Column(Integer)
@@ -37,7 +45,7 @@ class Conversation(Base):
         }
 class Chat_Messages (Base):
     __tablename__ = 'chat_messages'
-    id = Column(String(18), primary_key=True, default=lambda: str(generate_id(length=18)))
+    id = Column(String(18), primary_key=True, default=lambda: str(generate_general_id(length=18)))
     conversationID = Column(String(255))
     timeStamp = Column(TIMESTAMP)
     query = Column(String(255))
@@ -47,8 +55,18 @@ class Chat_Messages (Base):
 
 class RetrieverDoc(Base):
     __tablename__ = 'retrieverDocs'
-    id = Column(String(255), primary_key=True, default=lambda: str(generate_id(length=18)))
+    id = Column(String(255), primary_key=True, default=lambda: str(generate_general_id(length=18)))
     content = Column(Text)
     knowledge_doc_name = Column(String(255))
     knowledgeBaseId = Column(String(255))
     messageId = Column(String(255))
+
+# 文档信息
+class DocInfo(Base):
+    __tablename__ = 'docsInfo'
+    id = Column(String(255), primary_key=True, default=lambda: str(generate_general_id(length=18)))
+    doc_name = Column(String(255))
+    knowledgeBaseId = Column(String(255))
+    create_time = Column(TIMESTAMP)
+    doc_type = Column(String(255))
+    doc_size = Column(Integer)
