fba23aaec19091a4f5c29aeb04fae037059408f0
markyangkp <markyangkp@outlook.com>
2024-10-02 21:58:39 +0800

    获取文件保存路径

--- backend/main.py
+++ backend/main.py
@@ -6,9 +6,15 @@ from fastapi.staticfiles import StaticFiles
 
 import uvicorn
 
+from pathlib import Path
 
 app = FastAPI()
 
+BASE_DIR = Path(__file__).resolve().parent
+UPLOAD_DIR = BASE_DIR / "documents_stored"
+
+from config.config import set_docs_path
+import config.config
 
 
 router = APIRouter()
@@ -21,6 +27,8 @@ app.include_router(router, prefix="/v1/api/mark", tags=["mark"])
 
 
 if __name__ == "__main__":
+  set_docs_path(UPLOAD_DIR)
+  print(config.config.DOCS_PATH)
   config = uvicorn.Config("main:app", host="0.0.0.0", port=9988, reload=True)
   server = uvicorn.Server(config)
   server.run()
