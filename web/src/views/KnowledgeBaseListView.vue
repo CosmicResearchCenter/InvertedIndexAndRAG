97120b1a7cc9e85a730fdbeb90d1222462a08c3c
markyangkp <markyangkp@outlook.com>
2024-10-24 21:41:34 +0800

    知识库管理界面

--- web/src/views/KnowledgeBaseListView.vue
+++ web/src/views/KnowledgeBaseListView.vue
@@ -1,28 +1,37 @@
 <template>
-  <el-space wrap>
-      <el-card v-for="i in 3" :key="i" class="box-card" style="width: 250px">
-          <template #header>
-              <div class="card-header">
-                  <span>Card name</span>
-                  <el-button class="button" text>Operation button</el-button>
-              </div>
-          </template>
-          <div v-for="o in 4" :key="o" class="text item">
-              {{ 'List item ' + o }}
-          </div>
-      </el-card>
-      <el-card class="new-base"  style="width: 200px;height: 200px;">
-          新建知识库
-      </el-card>
-  </el-space>
+    <el-row :gutter="20">
+        <el-col :span="8" class="col-card" >
+            <el-card class="new-base" shadow="hover">
+                <el-icon :size="60"><Plus /></el-icon>
+            </el-card>
+        </el-col>
+        <el-col v-for="i in 10" :key="i" :span="8" class="col-card">
+            <el-card class="box-card" shadow="hover">
+                <div class="card-header">
+                    <span>Card name</span>
+                    <el-button class="button" text>Operation button</el-button>
+                </div>
+            </el-card>
+        </el-col>
+    </el-row>
 </template>
+
 <style scoped>
-.new-base{
-    /* background-color: #f5f7fa; */
-    /* border: 1px dashed #e4e7ed; */
+.box-card {
+    width: 100%;
+    height: 200px;
+}
+
+.new-base {
+    width: 100%;
+    height: 200px;
     border-radius: 6px;
     display: flex;
     justify-content: center;
     align-items: center;
+}
+
+.col-card {
+    margin-bottom: 20px; /* 行与行之间的间隔 */
 }
 </style>
