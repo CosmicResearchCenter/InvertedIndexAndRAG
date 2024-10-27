1df40bb2b2befec9685f4c9a8cf72dd82b22385d
markyangkp <markyangkp@outlook.com>
2024-10-27 20:38:37 +0800

    更新嵌入类型配置及异常信息

--- backend/app/core/rag/embedding/embedding_manager.py
+++ backend/app/core/rag/embedding/embedding_manager.py
@@ -4,23 +4,23 @@ from enum import Enum
 from .embedding import Embedding
 
 class EmbeddingType(Enum):
-    doubao = "DOUBAO"
-    openai = "OPENAI"
+    DOUBAO = "DOUBAO"
+    OPENAI = "OPENAI"
 
     @classmethod
     def get_embedding(cls, name: str):
+        print(f"Trying to get embedding for name: {name}")
         for member_name, member in cls.__members__.items():
             if member_name == name:
                 return member
         else:
             raise Exception("Not supported embedding type")
-
 class EmbeddingManager:
     def create_embedding(self, name: str)->Embedding:
         embedding_type = EmbeddingType.get_embedding(name)
-        if embedding_type == EmbeddingType.doubao:
+        if embedding_type == EmbeddingType.DOUBAO:
             return DouBaoEmbedding()
-        elif embedding_type == EmbeddingType.openai:
+        elif embedding_type == EmbeddingType.OPENAI:
             return OpenAIEmbedding()
         else:
-            raise Exception("Not supported embedding type")
+            raise Exception("Not supported embedding type Now")
