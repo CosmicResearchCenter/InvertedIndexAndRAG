36ed584096fc51218430e4d4777682bb11be9f45
markyangkp <markyangkp@outlook.com>
2024-11-14 15:48:21 +0800

    更新实体关系提取提示，修改输出格式为纯JSON并明确不需重复提取已提取的实体关系

--- backend/app/core/graphrag/prompt_template/prompts.py
+++ backend/app/core/graphrag/prompt_template/prompts.py
@@ -68,15 +68,19 @@ People,School,Skill,Hobby
 entity_relation_prompt = """
 请基于给定的实体和文本，提取并构建实体之间的关系，以支持知识图谱的构建。请遵循输出格式输出。
 ########################################################
-输出格式(csv格式文本):
+输出格式(纯json格式的文本):
 正确输出格式:
-实体1的名字 , 关系, 实体2的名字
+[
+    {"name1":"实体名","entity_lable1":"实体标签","relation":"关系","name2":"实体名","entity_lable2":"实体标签"}
+]
 错误输出格式:
-```csv
-实体1的名字 , 关系, 实体2的名字
+```json
+[
+    {"name1":"实体名","entity_lable1":"实体标签","relation":"关系","name2":"实体名","entity_lable2":"实体标签"}
+]
 ```
 ########################################################
-下面是已经提取出的实体节点，请不要重复提取:
+下面是已经提取出的实体节点关系，请不要重复提取:
 {{entity_relation_list}}
 ########################################################
 示例实体:
@@ -96,15 +100,17 @@ entity_relation_prompt = """
 小明出生于2000年一月五日，高中毕业于京海市第一中学，大学毕业于清华大学。他在大学里喜欢上了他们班的小美，但是被他们班另一个同学小王追上了小美。小明喜欢打篮球，摄影。他有许多技能，比如修电脑、会写代码。
 ########################################################
 示例输出:
-name1,entity_lable1, relation, name2,entity_lable2
-小明,People,就读,京海市第一中学,School
-小明,People,就读,清华大学,School
-小明,People,喜欢,小美,People
-小明,People,喜欢,打篮球,Hobby
-小明,People,喜欢,摄影,Hobby
-小王,People,追求,小美,People
-小明,People,具备技能,修电脑,Skill
-小明,People,具备技能,写代码,Skill
+[
+    {"name1":"小明","entity_lable1":"People","relation":"喜欢","name2":"小美","entity_lable2":"People"},
+    {"name1":"小明","entity_lable1":"People","relation":"同班同学","name2":"小美","entity_lable2":"People"},
+    {"name1":"小王","entity_lable1":"People","relation":"追求","name2":"小美","entity_lable2":"People"},
+    {"name1":"小明","entity_lable1":"People","relation":"喜欢","name2":"打篮球","entity_lable2":"Hobby"},
+    {"name1":"小明","entity_lable1":"People","relation":"喜欢","name2":"摄影","entity_lable2":"Hobby"},
+    {"name1":"小明","entity_lable1":"People","relation":"拥有技能","name2":"修电脑","entity_lable2":"Skill"},
+    {"name1":"小明","entity_lable1":"People","relation":"拥有技能","name2":"写代码","entity_lable2":"Skill"},
+    {"name1":"小明","entity_lable1":"People","relation":"高中毕业","name2":"京海市第一中学","entity_lable2":"School"},
+    {"name1":"小明","entity_lable1":"People","relation":"大学毕业","name2":"清华大学","entity_lable2":"School"}
+]
 ########################################################
 给定实体:
 {{entity_list}}
