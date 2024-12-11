7083b5e97acaf1654763a3ffecd9622076a98f2a
markyangkp <markyangkp@outlook.com>
2024-12-11 18:43:37 +0800

    移除数据库配置文件中的硬编码数据库连接信息，改为从环境变量加载数据库配置

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,25 +1,36 @@
 from logging.config import fileConfig
-
+import os
+from dotenv import load_dotenv
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
-from alembic import context
 from app.core.database.models import Base
+from alembic import context
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
+# 加载 .env 文件
+load_dotenv()
+
+MYSQL_IP = os.getenv("MYSQL_IP")
+MYSQL_PORT = os.getenv("MYSQL_PORT")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_BASE = os.getenv("MYSQL_BASE")
+DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_IP}:{MYSQL_PORT}/{MYSQL_BASE}"
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
